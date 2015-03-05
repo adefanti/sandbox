ff1f19d1c183bf1685aa134e10a5f82d35e93fe1
adefanti <aurelien.defanti@gmail.com>
2015-03-06 00:39:35 +0100

    Change RequestMapping from '/' to '/hello' and add requestParam 'name'

--- spring-boot-sandbox/src/main/java/org/sandbox/spring/controller/HelloController.java
+++ spring-boot-sandbox/src/main/java/org/sandbox/spring/controller/HelloController.java
@@ -1,13 +1,16 @@
 package org.sandbox.spring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    String sayHello() {
-        return "Hello world";
+	private static final String template = "Hello %s";
+
+    @RequestMapping("/hello")
+    String sayHello(@RequestParam(value="name", required=false, defaultValue="world") String name) {
+        return String.format(template, name);
     }
 }
